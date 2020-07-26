ff4327a19dace24ad582d4abe3f61fb60aae1792
KeironO <keiron.oshea@gmail.com>
2020-07-26 19:48:09 +0100

    Added more stuff

--- src/main.py
+++ src/main.py
@@ -12,13 +12,3 @@ except ImportError:
 
 import phonenumbers
 
-class Rejected(dbus.DBusException):
-  _dbus_error_name = "org.phonenumbers.Error.Rejected"
-
-class Endpoint(dbus.service.Object):
-  exit_on_release = True
-
-  def set_exit_on_release(self, exit_on_release):
-    self.exit_on_release = exit_on_release
-
-  @dbus.service.method("org.phonenumbers.Endpoint1", in_signature="", out_signature="")
